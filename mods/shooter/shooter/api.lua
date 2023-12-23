efbc75d0ab1723e4bb5fadc4103c79004ed59478
5uper1ach <5uper1achXD@gmail.com>
2023-12-23 11:55:49 +0100

    Add game

---
+++ mods/shooter/shooter/api.lua
@@ -0,0 +1,440 @@
+--[[
+Shooter API [shooter]
+Copyright (C) 2013-2019 stujones11, Stuart Jones
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+]]--
+
+shooter = {
+	registered_weapons = {},
+}
+
+shooter.config = {
+	automatic_weapons = true,
+	admin_weapons = false,
+	enable_blasting = false,
+	enable_particle_fx = true,
+	enable_protection = false,
+	enable_crafting = true,
+	explosion_texture = "shooter_hit.png",
+	node_drops = false,
+	allow_nodes = true,
+	allow_entities = false,
+	allow_players = true,
+	rounds_update_time = 0.4,
+	camera_height = 1.5,
+}
+
+shooter.default_particles = {
+	amount = 15,
+	time = 0.3,
+	minpos = {x=-0.1, y=-0.1, z=-0.1},
+	maxpos = {x=0.1, y=0.1, z=0.1},
+	minvel = {x=-1, y=1, z=-1},
+	maxvel = {x=1, y=2, z=1},
+	minacc = {x=-2, y=-2, z=-2},
+	maxacc = {x=2, y=-2, z=2},
+	minexptime = 0.1,
+	maxexptime = 0.75,
+	minsize = 1,
+	maxsize = 2,
+	collisiondetection = false,
+	texture = "shooter_hit.png",
+}
+
+local shots = {}
+local shooting = {}
+local config = shooter.config
+local server_step = minetest.settings:get("dedicated_server_step")
+
+shooter.register_weapon = function(name, def)
+	-- Fix definition table
+	def.sounds = def.sounds or {}
+	def.sounds.reload = def.sounds.reload or "shooter_reload"
+	def.sounds.fail_shot = def.sounds.fail_shot or "shooter_click"
+	def.reload_item = def.reload_item or "shooter:ammo"
+	def.spec.tool_caps.full_punch_interval = math.max(server_step,
+		def.spec.tool_caps.full_punch_interval)
+	def.spec.wear = math.ceil(65535 / def.spec.rounds)
+	def.spec.unloaded_item = name
+	def.unloaded_item = def.unloaded_item or {
+		description = def.description.." (unloaded)",
+		inventory_image = def.inventory_image,
+	}
+	shooter.registered_weapons[name] = table.copy(def)
+	-- Register loaded item tool
+	minetest.register_tool(name.."_loaded", {
+		description = def.description,
+		inventory_image = def.inventory_image,
+		on_use = function(itemstack, user, pointed_thing)
+			if type(def.on_use) == "function" then
+				itemstack = def.on_use(itemstack, user, pointed_thing)
+			end
+			if itemstack then
+				local spec = table.copy(def.spec)
+				if shooter.fire_weapon(user, itemstack, spec) then
+					itemstack:add_wear(def.spec.wear)
+					if itemstack:get_count() == 0 then
+						itemstack:replace(def.unloaded_item.name)
+					end
+				end
+			end
+			return itemstack
+		end,
+		on_hit = def.on_hit,
+		groups = {not_in_creative_inventory=1},
+	})
+	-- Register unloaded item tool
+	minetest.register_tool(name, {
+		description = def.unloaded_item.description,
+		inventory_image = def.unloaded_item.inventory_image,
+		groups = def.unloaded_item.groups or {},
+		on_use = function(itemstack, user)
+			local inv = user:get_inventory()
+			if inv then
+				local stack = def.reload_item
+				if inv:contains_item("main", stack) then
+					minetest.sound_play(def.sounds.reload, {object=user})
+					inv:remove_item("main", stack)
+					itemstack:replace(name.."_loaded 1 1")
+				else
+					minetest.sound_play(def.sounds.fail_shot, {object=user})
+				end
+			end
+			return itemstack
+		end,
+	})
+end
+
+shooter.get_configuration = function(conf)
+	for k, v in pairs(conf) do
+		local setting = minetest.settings:get("shooter_"..k)
+		if type(v) == "number" then
+			setting = tonumber(setting)
+		elseif type(v) == "boolean" then
+			setting = minetest.settings:get_bool("shooter_"..k)
+		end
+		if setting ~= nil then
+			conf[k] = setting
+		end
+	end
+	return conf
+end
+
+shooter.spawn_particles = function(pos, particles)
+	particles = particles or {}
+	if not config.enable_particle_fx == true or particles.amount == 0 then
+		return
+	end
+	local copy = function(v)
+		return type(v) == "table" and table.copy(v) or v
+	end
+	local p = {}
+	for k, v in pairs(shooter.default_particles) do
+		p[k] = particles[k] and copy(particles[k]) or copy(v)
+	end
+	p.minpos = vector.subtract(pos, p.minpos)
+	p.maxpos = vector.add(pos, p.maxpos)
+	minetest.add_particlespawner(p)
+end
+
+shooter.play_node_sound = function(node, pos)
+	local item = minetest.registered_items[node.name]
+	if item then
+		if item.sounds then
+			local spec = item.sounds.dug
+			if spec then
+				spec.pos = pos
+				minetest.sound_play(spec.name, spec)
+			end
+		end
+	end
+end
+
+shooter.is_valid_object = function(object)
+	if object then
+		if object:is_player() == true then
+			return config.allow_players
+		end
+		if config.allow_entities == true then
+			local luaentity = object:get_luaentity()
+			if luaentity then
+				return luaentity.name ~= "__builtin:item"
+			end
+		end
+	end
+end
+
+shooter.punch_node = function(pos, spec)
+	if config.enable_protection and minetest.is_protected(pos, spec.user) then
+		return
+	end
+	local node = minetest.get_node(pos)
+	if not node then
+		return
+	end
+	local item = minetest.registered_items[node.name]
+	if not item then
+		return
+	end
+	if item.groups then
+		for k, v in pairs(spec.groups) do
+			local level = item.groups[k] or 0
+			if level >= v then
+				minetest.remove_node(pos)
+				shooter.play_node_sound(node, pos)
+				if item.tiles then
+					if item.tiles[1] then
+						shooter.spawn_particles(pos, {texture=item.tiles[1]})
+					end
+				end
+				if config.node_drops then
+					local object = minetest.add_item(pos, item)
+					if object then
+						object:set_velocity({
+							x = math.random(-1, 1),
+							y = 4,
+							z = math.random(-1, 1)
+						})
+					end
+				end
+				return true
+			end
+		end
+	end
+end
+
+shooter.punch_object = function(object, tool_caps, dir, on_blast)
+	local do_damage = true
+	if on_blast and not object:is_player() then
+		local ent = object:get_luaentity()
+		if ent then
+			local def = minetest.registered_entities[ent.name] or {}
+			if def.on_blast then
+				do_damage = def.on_blast(ent, tool_caps.fleshy)
+			end
+		end
+	end
+	if do_damage then
+		object:punch(object, nil, tool_caps, dir)
+		return true
+	end
+end
+
+local function process_hit(pointed_thing, spec, dir)
+	local def = minetest.registered_items[spec.name] or {}
+	if type(def.on_hit) == "function" then
+		if def.on_hit(pointed_thing, spec, dir) == true then
+			return
+		end
+	end
+	if pointed_thing.type == "node" and config.allow_nodes == true then
+		local pos = minetest.get_pointed_thing_position(pointed_thing, false)
+		shooter.punch_node(pos, spec)
+	elseif pointed_thing.type == "object" then
+		local object = pointed_thing.ref
+		if shooter.is_valid_object(object) and
+				shooter.punch_object(object, spec.tool_caps, dir) then
+			local pos = pointed_thing.intersection_point or object:get_pos()
+			local groups = object:get_armor_groups() or {}
+			if groups.fleshy then
+				shooter.spawn_particles(pos, spec.particles)
+			end
+		end
+	end
+end
+
+local function process_round(round)
+	round.dist = round.dist + round.spec.step
+	if round.dist > round.spec.range then
+		return
+	end
+	local p1 = round.pos
+	local p2 = vector.add(p1, vector.multiply(round.dir, round.spec.step))
+	local ray = minetest.raycast(p1, p2, true, true)
+	local pointed_thing = ray:next() or {type="nothing"}
+	if pointed_thing.type ~= "nothing" then
+		return process_hit(pointed_thing, round.spec, round.dir)
+	end
+	round.pos = p2
+	minetest.after(shooter.config.rounds_update_time, function(...)
+		process_round(...)
+	end, round)
+end
+
+local function fire_weapon(player, itemstack, spec, extended)
+	if not player then
+		return
+	end
+	local dir = player:get_look_dir()
+	local pos = player:get_pos()
+	if not dir or not pos then
+		return
+	end
+	pos.y = pos.y + config.camera_height
+	spec.origin = vector.add(pos, dir)
+	local interval = spec.tool_caps.full_punch_interval
+	shots[spec.user] = minetest.get_us_time() / 1000000 + interval
+	minetest.sound_play(spec.sound, {object=player})
+	if spec.bullet_image then
+		minetest.add_particle({
+			pos = pos,
+			velocity = vector.multiply(dir, 30),
+			acceleration = {x=0, y=0, z=0},
+			expirationtime = 0.5,
+			size = 0.25,
+			texture = spec.bullet_image,
+		})
+	end
+	process_round({
+		spec = spec,
+		pos = vector.new(spec.origin),
+		dir = dir,
+		dist = 0,
+	})
+	if extended then
+		itemstack:add_wear(spec.wear)
+		if itemstack:get_count() == 0 then
+			itemstack = spec.unloaded_item
+			player:set_wielded_item(itemstack)
+			return
+		end
+		player:set_wielded_item(itemstack)
+	end
+	if not spec.automatic or not shooting[spec.user] then
+		return
+	end
+	minetest.after(interval, function(...)
+		if shooting[spec.user] then
+			local arg = {...}
+			fire_weapon(arg[1], arg[2], arg[3], true)
+		end
+	end, player, itemstack, spec)
+end
+
+shooter.fire_weapon = function(player, itemstack, spec)
+	local name = player:get_player_name()
+	local time = minetest.get_us_time() / 1000000
+	if shots[name] and time <= shots[name] then
+		return false
+	end
+	if config.automatic_weapons then
+		if config.admin_weapons and minetest.check_player_privs(name,
+				{server=true}) then
+			spec.automatic = true
+		end
+		shooting[name] = true
+	end
+	spec.user = name
+	spec.name = itemstack:get_name()
+	fire_weapon(player, itemstack, spec)
+	return true
+end
+
+shooter.blast = function(pos, radius, fleshy, distance, user)
+	if not user then
+		return
+	end
+	pos = vector.round(pos)
+	local name = user:get_player_name()
+	local p1 = vector.subtract(pos, radius)
+	local p2 = vector.add(pos, radius)
+	minetest.sound_play("shooter_explode", {
+		pos = pos,
+		gain = 10,
+		max_hear_distance = 100
+	})
+	if config.allow_nodes and config.enable_blasting then
+		if not config.enable_protection or
+				not minetest.is_protected(pos, name) then
+			minetest.set_node(pos, {name="shooter:boom"})
+		end
+	end
+	if config.enable_particle_fx == true then
+		minetest.add_particlespawner({
+			amount = 50,
+			time = 0.1,
+			minpos = p1,
+			maxpos = p2,
+			minvel = {x=0, y=0, z=0},
+			maxvel = {x=0, y=0, z=0},
+			minacc = {x=-0.5, y=5, z=-0.5},
+			maxacc = {x=0.5, y=5, z=0.5},
+			minexptime = 0.1,
+			maxexptime = 1,
+			minsize = 8,
+			maxsize = 15,
+			collisiondetection = false,
+			texture = "shooter_smoke.png",
+		})
+	end
+	local objects = minetest.get_objects_inside_radius(pos, distance)
+	for _,obj in ipairs(objects) do
+		if shooter.is_valid_object(obj) then
+			local obj_pos = obj:get_pos()
+			local dist = vector.distance(obj_pos, pos)
+			local damage = (fleshy * 0.5 ^ dist) * 2
+			if dist ~= 0 then
+				obj_pos.y = obj_pos.y + 1
+				local blast_pos = {x=pos.x, y=pos.y + 4, z=pos.z}
+				if shooter.is_valid_object(obj) and
+						minetest.line_of_sight(obj_pos, blast_pos, 1) then
+					shooter.punch_object(obj, {
+						full_punch_interval = 1.0,
+						damage_groups = {fleshy=damage},
+					}, nil, true)
+				end
+			end
+		end
+	end
+	if config.allow_nodes and config.enable_blasting then
+		local pr = PseudoRandom(os.time())
+		local vm = VoxelManip()
+		local min, max = vm:read_from_map(p1, p2)
+		local area = VoxelArea:new({MinEdge=min, MaxEdge=max})
+		local data = vm:get_data()
+		local c_air = minetest.get_content_id("air")
+		for z = -radius, radius do
+			for y = -radius, radius do
+				local vp = {x=pos.x - radius, y=pos.y + y, z=pos.z + z}
+				local vi = area:index(vp.x, vp.y, vp.z)
+				for x = -radius, radius do
+					if (x * x) + (y * y) + (z * z) <=
+							(radius * radius) + pr:next(-radius, radius) then
+						if config.enable_protection then
+							if not minetest.is_protected(vp, name) then
+								data[vi] = c_air
+							end
+						else
+							data[vi] = c_air
+						end
+					end
+					vi = vi + 1
+				end
+			end
+		end
+		vm:set_data(data)
+		vm:update_liquids()
+		vm:write_to_map()
+	end
+end
+
+shooter.get_shooting = function(name)
+	return shooting[name]
+end
+
+shooter.set_shooting = function(name, is_shooting)
+	shooting[name] = is_shooting and true or nil
+end
---
+++ mods/szombie_core/init.lua
@@ -0,0 +1,149 @@
+local hud_id
+
+minetest.register_on_newplayer(function(player)
+    player:get_inventory():add_item("main", "shooter:machine_gun")
+    while player:get_inventory():add_item("main", "shooter:ammo 99"):get_count() == 0 do end
+end)
+
+minetest.register_on_joinplayer(function(player, last_login)
+    player:set_sky({ type = "skybox", textures = { "skybox_down.png", "skybox_up.png", "skybox_left.png", "skybox_right.png", "skybox_back.png", "skybox_front.png"}})
+    -- Gregor ist cool
+    -- minetest.sound_play("aargh", { to_player = player:get_player_name(), loop = true })
+    hud_id = player:hud_add({
+        hud_elem_type = "text",
+        position = {x = 0, y = 1},
+        text = "0",
+        scale = {x = 10000000, y = 100000},
+        alignment = {x = 1,y = -1},
+        size = {x = 5, y = 0},
+        number = 0xFFFFFF,
+        offset = { x = 10, y = -10},
+    })
+end)
+
+minetest.register_node("szombie_core:stone", {
+    tiles = { "trak2_tile1b.tga" },
+})
+
+minetest.set_mapgen_setting("mg_name", "singlenode", true)
+minetest.register_alias_force("mapgen_singlenode", "air")
+
+local vm_data
+
+minetest.register_on_generated(function(pos_min, pos_max)
+    local vm, vm_pos_min, vm_pos_max = minetest.get_mapgen_object("voxelmanip")
+    local vm_area = VoxelArea:new{MinEdge = vm_pos_min, MaxEdge = vm_pos_max}
+    vm_data = vm:get_data(vm_data)
+
+    local stone = minetest.get_content_id("szombie_core:stone")
+
+    for x = pos_min.x, pos_max.x do
+        for y = pos_min.y, pos_max.y do
+            for z = pos_min.z, pos_max.z do
+                local index = vm_area:index(x, y, z)
+
+                if y <= -3 then
+                    vm_data[index] = stone
+                end
+            end
+        end
+    end
+
+    vm:set_data(vm_data)
+    vm:write_to_map()
+end)
+
+local function spawn_monster(player)
+    local position_auf_kreis = math.random(-2 * math.pi, 2 * math.pi)
+    local distanz = math.random(10, 20)
+    local spieler_position = player:get_pos()
+    local position_in_welt = vector.new(
+        spieler_position.x + math.sin(position_auf_kreis) * distanz,
+        0,
+        spieler_position.z + math.cos(position_auf_kreis) * distanz
+    )
+    minetest.add_entity(position_in_welt, "mobs_monster:dirt_monster")
+end
+
+local hud_blinking = false
+local hud_blinking_t = 0
+
+local SPAWN_RATE = 10
+local dtime_accu = SPAWN_RATE
+
+minetest.register_globalstep(function(dtime)
+    local players = minetest.get_connected_players()
+    if #players == 0 then
+        return
+    end
+
+    for _, player in ipairs(players) do
+        if not hud_blinking then
+            player:hud_change(hud_id, "number", "0xFFFFFF")
+            player:hud_change(hud_id, "size", {x = 5})
+        else
+            hud_blinking_t = hud_blinking_t + dtime
+            if hud_blinking_t < 2 then
+                if math.round(minetest.get_us_time() / 250000) % 2 == 0 then
+                    player:hud_change(hud_id, "number", "0xFF0000")
+                    player:hud_change(hud_id, "size", {x = 30})
+                else
+                    player:hud_change(hud_id, "number", "0xFFFFFF")
+                    player:hud_change(hud_id, "size", {x = 30})
+                end
+            else
+                hud_blinking_t = 0
+                hud_blinking = false
+                player:hud_change(hud_id, "number", "0xFFFFFF")
+                player:hud_change(hud_id, "size", {x = 5})
+            end
+        end
+    end
+
+    dtime_accu = dtime_accu + dtime
+    if dtime_accu < SPAWN_RATE then
+        return
+    end
+    dtime_accu = 0
+
+    for _, player in ipairs(players) do
+        for i = 1, 15 do
+            spawn_monster(player)
+        end
+    end
+end)
+
+local dead_monsters = 0
+
+local function update_hud(player)
+    player:hud_change(hud_id, "text", tostring(dead_monsters))
+end
+
+szombie_core = {}
+szombie_core.on_monster_die = function()
+    dead_monsters = dead_monsters + 1
+    update_hud(minetest.get_connected_players()[1])
+    hud_blinking = true
+end
+
+minetest.register_on_respawnplayer(function()
+    minetest.clear_objects()
+    dtime_accu = SPAWN_RATE
+    dead_monsters = 0
+    update_hud(minetest.get_connected_players()[1])
+end)
+
+-- Override the hand item registered in the engine in builtin/game/register.lua
+minetest.override_item("", {
+	wield_scale = {x=1,y=1,z=2.5},
+	tool_capabilities = {
+		full_punch_interval = 0,
+		max_drop_level = 0,
+		groupcaps = {
+			crumbly = {times={[2]=3.00, [3]=0.70}, uses=0, maxlevel=1},
+			snappy = {times={[3]=0.40}, uses=0, maxlevel=1},
+			oddly_breakable_by_hand = {times={[1]=3.50,[2]=2.00,[3]=0.70}, uses=0}
+		},
+		damage_groups = {fleshy=1},
+	}
+})
