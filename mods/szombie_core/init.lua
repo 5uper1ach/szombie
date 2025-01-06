4939514ec9f67ea0cf66e21e9a59c322b9746b63
5uper1ach <5uper1achxd@gmail.com>
2025-01-06 20:21:07 +0100

    Leave hand up to MTG mods
    makes creative hand work

--- mods/szombie_core/init.lua
+++ mods/szombie_core/init.lua
@@ -196,18 +196,3 @@ minetest.register_on_respawnplayer(function()
     dead_monsters = 0
     update_hud(minetest.get_connected_players()[1])
 end)
-
--- Override the hand item registered in the engine in builtin/game/register.lua
-minetest.override_item("", {
-	wield_scale = {x=1,y=1,z=2.5},
-	tool_capabilities = {
-		full_punch_interval = 0,
-		max_drop_level = 0,
-		groupcaps = {
-			crumbly = {times={[2]=3.00, [3]=0.70}, uses=0, maxlevel=1},
-			snappy = {times={[3]=0.40}, uses=0, maxlevel=1},
-			oddly_breakable_by_hand = {times={[1]=3.50,[2]=2.00,[3]=0.70}, uses=0}
-		},
-		damage_groups = {fleshy=1},
-	}
-})
