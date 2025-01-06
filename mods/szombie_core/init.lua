2d0fc80193eca9c28c9a87a5c4d424297be4a143
grorp <grorp@posteo.de>
2025-01-06 23:07:15 +0100

    Enable creative mode for prototyping

--- mods/szombie_core/init.lua
+++ mods/szombie_core/init.lua
@@ -1,3 +1,4 @@
 dofile(core.get_modpath(core.get_current_modname()) .. "/env.lua")
 dofile(core.get_modpath(core.get_current_modname()) .. "/mapgen.lua")
-dofile(core.get_modpath(core.get_current_modname()) .. "/gamelogic.lua")
+-- disabled for prototyping
+-- dofile(core.get_modpath(core.get_current_modname()) .. "/gamelogic.lua")
